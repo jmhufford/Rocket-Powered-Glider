4265a357ef3b36667f0dd208c1129b5967596b47
Jerome <jerome525@gmail.com>
2019-03-10 23:45:36 -0400

    Initial Commit

---
+++ RPG Orange/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  RPG Orange
+//
+//  Created by Jerome - UCF on 3/10/19.
+//  Copyright © 2019 Jerome.gg. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
